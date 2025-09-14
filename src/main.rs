9691b15d45cc0199519794399bcce976ba679dd7
Shawon <mdmoinulhossainshawon@gmail.com>
2025-09-14 16:03:46 +0600

    chore: Initial commit

---
+++ src/main.rs
@@ -0,0 +1,184 @@
+use crossterm::{
+    cursor::{Hide, MoveToColumn, MoveUp, Show}, 
+    event::{self, Event, KeyCode, KeyModifiers},
+    execute,
+    terminal::{self, Clear, ClearType}
+};
+use std::{io::{stdout, Write}, time::Duration};
+
+mod term;
+
+/// Show the help message.
+fn print_help () {
+    println!("");
+    println!(
+        "{}{}Fidget{}{}: {}Fidget spinners for the terminal!",
+        term::bold(),
+        term::term_hex("#a6e3a1"),
+
+        term::reset(),
+        term::term_hex("#9399b2"),
+
+        term::reset(),
+    );
+
+    //|fS "chunk: Arguments"
+
+    println!("");
+    println!("Usage,");
+    println!("");
+    println!(
+        "   {}fidget {}<command> {}<name> {}...{}",
+        term::term_hex("#89b4fa"),
+        term::term_hex("#eba0ac"),
+        term::term_hex("#f5c2e7"),
+        term::term_hex("#9399b2"),
+
+        term::reset(),
+    );
+    println!("");
+    println!(
+        "   {}{}<command>    {}Either {}show{} or {}output{}.",
+        term::bold(),
+        term::term_hex("#eba0ac"),
+
+        term::reset(),
+
+        term::term_hex("#cba6f7"),
+        term::reset(),
+
+        term::term_hex("#cba6f7"),
+        term::reset(),
+    );
+    println!(
+        "   {}{}<name>       {}Fidget name",
+        term::bold(),
+        term::term_hex("#f5c2e7"),
+        term::reset(),
+    );
+    println!(
+        "   {}{}...          {}Options",
+        term::bold(),
+        term::term_hex("#9399b2"),
+        term::reset(),
+    );
+
+    //|fE
+
+    //|fS "chunk: Fidget names"
+
+    println!("");
+    println!("Fidget names,");
+    println!("");
+
+    println!(
+        "   {}{}default      {}Basic spinner",
+        term::bold(),
+        term::term_hex("#eba0ac"),
+        term::reset(),
+    );
+
+    //|fE
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+fn draw (name: &str) {
+    fn clear () {
+        execute!(
+            stdout(),
+
+            MoveUp(2),
+            Clear(ClearType::FromCursorDown)
+        ).ok();
+    }
+
+    fn draw (name: &str, states: &Vec<&str>, mut index: u32) -> u32 {
+        let mut _index = (index + 1) as usize;
+
+        if _index >= states.len() {
+            index = 0;
+            _index = 0_usize;
+        } else {
+            index += 1;
+        }
+
+        execute!(stdout(),MoveToColumn(0)).ok();
+        println!(
+            "Style: {}{}{}{}    Frame: {}{}{}{}    Total: {}{}{}{}",
+
+            term::term_hex("#a6e3a1"),
+            term::bold(),
+            &name,
+            term::reset(),
+
+            term::term_hex("#fab387"),
+            term::bold(),
+            &index,
+            term::reset(),
+
+            term::term_hex("#89b4fa"),
+            term::bold(),
+            states.len(),
+            term::reset(),
+        );
+
+        execute!(stdout(), MoveToColumn(0)).ok();
+        println!("{}", states[_index]);
+
+        index
+    }
+
+    let states = match name {
+        "default" => vec![ "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", " " ],
+        _ => vec![]
+    };
+
+    if states.len() == 0 {
+        return;
+    }
+
+    execute!(stdout(), Hide).ok();
+    println!("");
+
+    let mut frame = draw(name, &states, 0);
+
+    loop {
+        if event::poll(Duration::from_millis(100)).unwrap() {
+            if let Event::Key(key) = event::read().unwrap() {
+                if key.code == KeyCode::Char('d') && key.modifiers == KeyModifiers::CONTROL {
+                    execute!(stdout(), Show).ok();
+                    return;
+                } else if key.code == KeyCode::Char('q') {
+                    execute!(stdout(), Show).ok();
+                    return;
+                } else {
+                    clear();
+                    frame = draw(name, &states, frame);
+                }
+            }
+        } else {
+            clear();
+            frame = draw(name, &states, frame);
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    terminal::enable_raw_mode()?;
+
+    let mut args = std::env::args();
+    let fidget_name: String = match args.nth(1) {
+        Some(n) => n,
+        None => String::from("")
+    };
+
+    if fidget_name == "" {
+        print_help();
+    } else {
+        draw(&fidget_name);
+    }
+
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
---
+++ src/term.rs
@@ -0,0 +1,38 @@
+pub fn term_rgb (r: u32, g: u32, b: u32) -> String {
+    format!("\x1b[38;2;{};{};{}m", r, g, b)
+}
+
+pub fn term_hex (hex: &str) -> String {
+    let inner = hex.trim_start_matches("#");
+
+    let r: u32;
+    let g: u32;
+    let b: u32;
+
+    match inner.chars().count() {
+        3 => {
+            r = u32::from_str_radix(&inner[0..1], 16).expect("Failed to get value of `r`");
+            g = u32::from_str_radix(&inner[1..2], 16).expect("Failed to get value of `g`");
+            b = u32::from_str_radix(&inner[2..3], 16).expect("Failed to get value of `b`");
+        },
+        6 => {
+            r = u32::from_str_radix(&inner[0..2], 16).expect("Failed to get value of `r`");
+            g = u32::from_str_radix(&inner[2..4], 16).expect("Failed to get value of `g`");
+            b = u32::from_str_radix(&inner[4..6], 16).expect("Failed to get value of `b`");
+        },
+
+        _ => {
+            r = 255;
+            g = 255;
+            b = 255;
+        }
+    }
+
+    format!("\x1b[38;2;{};{};{}m", r, g, b)
+}
+
+pub fn reset () -> String { "\x1b[0m".to_string() }
+pub fn bold () -> String { "\x1b[1m".to_string() }
+
+/// Hello
+pub fn italic () -> String { "\x1b[3m".to_string() }
