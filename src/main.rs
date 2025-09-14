fcfa0824a922c90e7e87634164d2e7e66de9c72d
Shawon <mdmoinulhossainshawon@gmail.com>
2025-09-14 17:09:22 +0600

    feat: Added more stats

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,7 @@ fn draw (name: &str) {
         ).ok();
     }
 
-    fn draw (name: &str, states: &Vec<&str>, mut index: u32) -> u32 {
+    fn draw (name: &str, states: &Vec<&str>, mut index: u32, update_delay: u32) -> u32 {
         let mut _index = (index + 1) as usize;
 
         if _index >= states.len() {
@@ -105,32 +105,50 @@ fn draw (name: &str) {
 
         execute!(stdout(),MoveToColumn(0)).ok();
         println!(
-            "Style: {}{}{}{}    Frame: {}{}{}{}    Total: {}{}{}{}",
+            "{}Style: {}{}{}{}{}    Frame: {}{}{}{}{}    Total: {}{}{}{}{}    Update: {}{}{}ms{}",
+
+            term::term_hex("#9399b2"),
 
             term::term_hex("#a6e3a1"),
             term::bold(),
             &name,
             term::reset(),
+            term::term_hex("#9399b2"),
 
             term::term_hex("#fab387"),
             term::bold(),
             &index,
             term::reset(),
+            term::term_hex("#9399b2"),
 
             term::term_hex("#89b4fa"),
             term::bold(),
             states.len(),
             term::reset(),
+            term::term_hex("#9399b2"),
+
+            term::term_hex("#89dceb"),
+            update_delay,
+            term::bold(),
+            term::reset(),
         );
 
         execute!(stdout(), MoveToColumn(0)).ok();
-        println!("{}", states[_index]);
+        println!(
+            "{}╰──╴{}{}{}",
+
+            term::term_hex("#9399b2"),
+            term::term_hex("#cba6f7"),
+            states[_index],
+            term::reset(),
+        );
 
         index
     }
 
     let states = match name {
         "default" => vec![ "▁", "▂", "▃", "▄", "▅", "▆", "▇", "█", " " ],
+        "loader" => vec![ "[----]", "[=---]", "[==--]", "[===-]", "[====]" ],
         _ => vec![]
     };
 
@@ -141,10 +159,11 @@ fn draw (name: &str) {
     execute!(stdout(), Hide).ok();
     println!("");
 
-    let mut frame = draw(name, &states, 0);
+    let mut update_delay = 100;
+    let mut frame = draw(name, &states, 0, update_delay);
 
     loop {
-        if event::poll(Duration::from_millis(100)).unwrap() {
+        if event::poll(Duration::from_millis(update_delay as u64)).unwrap() {
             if let Event::Key(key) = event::read().unwrap() {
                 if key.code == KeyCode::Char('d') && key.modifiers == KeyModifiers::CONTROL {
                     execute!(stdout(), Show).ok();
@@ -152,14 +171,24 @@ fn draw (name: &str) {
                 } else if key.code == KeyCode::Char('q') {
                     execute!(stdout(), Show).ok();
                     return;
+                } else if key.code == KeyCode::Char('h') && update_delay > 100 {
+                    update_delay -= 50;
+
+                    clear();
+                    frame = draw(name, &states, frame, update_delay);
+                } else if key.code == KeyCode::Char('l') && update_delay < 1000 {
+                    update_delay += 50;
+
+                    clear();
+                    frame = draw(name, &states, frame, update_delay);
                 } else {
                     clear();
-                    frame = draw(name, &states, frame);
+                    frame = draw(name, &states, frame, update_delay);
                 }
             }
         } else {
             clear();
-            frame = draw(name, &states, frame);
+            frame = draw(name, &states, frame, update_delay);
         }
     }
 }
